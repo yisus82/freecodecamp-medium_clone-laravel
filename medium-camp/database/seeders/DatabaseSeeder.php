b10bd20696256ec721e8a785a176b95cbe0b5352
yisus82 <djalma8@gmail.com>
2025-10-22 13:24:49 +0200

    Modify user seeder

--- medium-camp/database/seeders/DatabaseSeeder.php
+++ medium-camp/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
 
         $categories = [
             'Technology',
