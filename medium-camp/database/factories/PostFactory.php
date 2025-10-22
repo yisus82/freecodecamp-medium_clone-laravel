4160f7c87ba2b524d214fe9519e95055d18b94aa
yisus82 <djalma8@gmail.com>
2025-10-22 11:00:31 +0200

    Factories & seeders

---
+++ medium-camp/database/factories/PostFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence();
+
+        return [
+            'title' => $title,
+            'slug' => \Str::slug($title),
+            'content' => $this->faker->paragraphs(5, true),
+            'image_path' => $this->faker->optional()->imageUrl(800, 600),
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'user_id' => User::inRandomOrder()->first()->id,
+            'published_at' => $this->faker->optional()->dateTimeBetween('-1 years', 'now'),
+        ];
+    }
+}
--- medium-camp/database/seeders/DatabaseSeeder.php
+++ medium-camp/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +17,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $categories = [
+            'Technology',
+            'Health',
+            'Science',
+            'Sports',
+            'Politics',
+            'Entertainment',
+        ];
+
+        foreach ($categories as $category) {
+            Category::create(['name' => $category]);
+        }
+
+        Post::factory(100)->create();
     }
 }
