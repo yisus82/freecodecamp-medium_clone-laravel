cd663833c45ffea298f0e02b727731cb30197a94
yisus82 <djalma8@gmail.com>
2025-11-06 12:48:22 +0100

    Change layouts

--- medium-camp/app/Http/Controllers/PostController.php
+++ medium-camp/app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::orderByDesc('published_at')->paginate(10);
-        return view('dashboard', compact('posts'));
+        return view('post.index', compact('posts'));
     }
 
     /**
--- medium-camp/routes/web.php
+++ medium-camp/routes/web.php
@@ -4,11 +4,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('post.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
